b5886d8427f57f6aabbe413482f4f8579261f011
coffeeknife <20546755+coffeeknife@users.noreply.github.com>
2024-09-23 15:27:04 -0500

    check if docker is installed; install if its not

--- stacks/docker/jellyfin/init_jellyfin.sh
+++ stacks/docker/jellyfin/init_jellyfin.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
-bash <(curl -fsSL https://get.docker.com) # install docker first
+if [ $(dpkg-query -W -f='${Status}' docker-ce 2>/dev/null | grep -c "ok installed") -eq 0 ];
+then
+  bash <(curl -fsSL https://get.docker.com)
+fi
 
