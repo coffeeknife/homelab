b91380636d8f7c4c48e6493bc66732e79036ffff
coffeeknife <20546755+coffeeknife@users.noreply.github.com>
2024-09-23 15:38:42 -0500

    add systemd service generator

--- stacks/docker/jellyfin/init_jellyfin.sh
+++ stacks/docker/jellyfin/init_jellyfin.sh
@@ -1,7 +1,36 @@
 #!/bin/bash
 
+apt install curl -y
+
 if [ $(dpkg-query -W -f='${Status}' docker-ce 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   bash <(curl -fsSL https://get.docker.com)
 fi
 
+if [$(systemctl is-active jellyfin) == "active"]; then
+  systemctl restart jellyfin
+  systemctl status jellyfin
+  exit 0
+else
+  # create service file for jellyfin
+  COMPOSEFILE="$(pwd)/docker-compose.yaml"
+  cat > /etc/systemd/system/jellyfin.service << EOF |
+  [Unit]
+  Description=Jellyfin Docker Compose Stack
+  After=docker.service
+  Requires=docker.service
+  
+  [Service]
+  Type=oneshot
+  RemainAfterExit=yes
+  ExecStart=/bin/bash -c "docker compose -f $COMPOSEFILE up --detach"
+  ExecStop=/bin/bash -c "docker compose -f $COMPOSEFILE stop"
+
+  [Install]
+  WantedBy=multi-user.target
+  EOF
+
+  echo 'file made'
+fi
+
+exit 0
