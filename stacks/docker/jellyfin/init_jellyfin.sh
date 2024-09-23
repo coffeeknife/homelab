b14e3b4bcabf519262c2ca0df175788b80984612
coffeeknife <20546755+coffeeknife@users.noreply.github.com>
2024-09-23 15:41:20 -0500

    oops

--- stacks/docker/jellyfin/init_jellyfin.sh
+++ stacks/docker/jellyfin/init_jellyfin.sh
@@ -5,6 +5,8 @@ apt install curl -y
 if [ $(dpkg-query -W -f='${Status}' docker-ce 2>/dev/null | grep -c "ok installed") -eq 0 ];
 then
   bash <(curl -fsSL https://get.docker.com)
+else
+  echo 'docker already installed, skipping'
 fi
 
 if [$(systemctl is-active jellyfin) == "active"]; then
@@ -14,7 +16,7 @@ if [$(systemctl is-active jellyfin) == "active"]; then
 else
   # create service file for jellyfin
   COMPOSEFILE="$(pwd)/docker-compose.yaml"
-  cat > /etc/systemd/system/jellyfin.service << EOF |
+  cat > /etc/systemd/system/jellyfin.service << EOF
   [Unit]
   Description=Jellyfin Docker Compose Stack
   After=docker.service
